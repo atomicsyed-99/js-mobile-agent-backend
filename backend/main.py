824fd330d2952295e89a940ef201760dba0229e5
Syed Ishtiyaq Ahmed <52957729+atomicsyed-99@users.noreply.github.com>
2025-04-15 11:58:46 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from openai import AzureOpenAI
 import os
-from load_dotenv import load_dotenv
+from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
